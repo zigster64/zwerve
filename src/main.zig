0ec25d9841a6a5bc3522fa5890bfaeeaf889f8e5
zigster64 <steve@raffmail.net>
2023-04-24 18:09:40 +1000

    basic init

---
+++ src/main.zig
@@ -0,0 +1,6 @@
+const std = @import("std");
+
+pub fn main() !void {
+    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
+    std.debug.print("All your {s} are belong to {s}.\n", .{ "drift", "zwerve" });
+}
